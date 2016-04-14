e5cff254277406fa194a742267e32ac6598a4955
Luwin <1219810894@qq.com>
2016-04-14 21:56:10 +0800

    add config example

--- config.example.py
+++ config.example.py
@@ -1,31 +1,22 @@
 # -*- coding: utf-8 -*-
-"""Copy this file to config/__init__.py and fill needed field values"""
-
+"""Copy this file to config.py and fill needed field values"""
 import os
 
-# Statement for enabling the development environment
-DEBUG = True
 
-# Define the application directory
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+    DEBUG = False
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = '/tmp/uploads'
+    TMP_UPLOAD_FOLDER = '/tmp/uploads/tmp'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
-# Define the database - we are working with
-# SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
 
-# Application threads. A common general assumption is
-# using 2 per available processor cores - to handle
-# incoming requests using one and performing background
-# operations using the other.
-THREADS_PER_PAGE = 2
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
 
-# Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED = True
 
-# Use a secure, unique and absolutely secret key for
-# signing the data.
-CSRF_SESSION_KEY = "secret"
-
-# Secret key for signing cookies
-SECRET_KEY = "secret"
+class DevelopmentConfig(Config):
+    # Upload file
+    DEBUG = True
