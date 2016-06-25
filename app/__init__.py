7edf5c8d69c6ece22f774b25681363cd2b4ede69
Luwin <1219810894@qq.com>
2016-06-25 22:35:19 +0800

    addllowross domain

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # Import flask and template operators
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-# from flask.ext.cors import CORS
+from flask.ext.cors import CORS
 from flask_socketio import SocketIO
 from flask_redis import Redis
 
@@ -17,7 +17,7 @@ app.config.from_object('config.DevelopmentConfig')
 socketio = SocketIO(app, async_mode=async_mode)
 db = SQLAlchemy(app)
 redis = Redis(app)
-# cors = CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
+cors = CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 
 
 import views, socket, api
